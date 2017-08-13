6e6b44c24d75c784dc8b5278db97aad1056463aa
Bilel <bn@fondative.com>
2017-08-13 16:08:38 +0100

    End of part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { Operation } from './common/operation.model';
+import { State, Store } from '@ngrx/store';
+import { ADD_OPERATION, DECREMENT_OPERATION, INCREMENT_OPERATION, REMOVE_OPERATION, } from './common/operations';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'app';
+    public id = 0;
+    public operations: Observable<Array<Operation>>;
+
+    // initialize a new operation class instance
+    public operation: Operation = new Operation();
+
+    constructor(private _store: Store<State<Array<Operation>>>) {
+        this.operations = _store.select('operations');
+    }
+
+    addOperation(operation) {
+        this._store.dispatch({type: ADD_OPERATION , payload: {
+            id: ++ this.id, // simulating ID increments
+            reason: operation.reason,
+            amount: operation.amount
+        }});
+    }
+
+    // Adding REMOVE_OPERATIOn , INCREMENT_OPERATION , DECREMENT_OPERATION
+    incrementOperation(operation) {
+        this._store.dispatch({type: INCREMENT_OPERATION, payload: operation})
+    }
+
+    decrementOperation(operation) {
+        this._store.dispatch({type: DECREMENT_OPERATION, payload: operation})
+    }
+
+    deleteOperation(operation) {
+        this._store.dispatch({type: REMOVE_OPERATION, payload: operation})
+    }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { operationsReducer } from './common/operations';
+import { FormsModule } from '@angular/forms';
+import { NewOperation } from './new-operation.component';
+import { OperationsList } from './operations-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+      NewOperation,
+      OperationsList
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      FormsModule,
+      StoreModule.provideStore({ operations: operationsReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
